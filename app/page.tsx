3dfbc7d2ec297cfe22f83cfc2b8fb953b39598a3
Andrew Rusli <andrewrusli@college.harvard.edu>
2026-09-11 21:41:57 +0900

    chore: replace site with redirect to arusli.dev

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Gallery } from "@/components/Gallery";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-paper">
-      <Gallery />
-    </main>
-  );
-}
